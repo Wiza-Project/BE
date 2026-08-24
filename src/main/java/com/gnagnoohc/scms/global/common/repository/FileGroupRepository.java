4f76933785b98db8a9e523587a7679768a51ec4d
Cloud-bb <taay_429@naver.com>
2026-08-24 11:28:56 +0900

    feat(WP-137): FileGroup/StoredFile JPA 리포지토리 추가

---
+++ src/main/java/com/gnagnoohc/scms/global/common/repository/FileGroupRepository.java
@@ -0,0 +1,7 @@
+package com.gnagnoohc.scms.global.common.repository;
+
+import com.gnagnoohc.scms.global.common.entity.FileGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileGroupRepository extends JpaRepository<FileGroup, Integer> {
+}
