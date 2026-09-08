77d8900591365979cdae892854023beec3a9146b
log-dh <ekgml085212@gmail.com>
2026-09-08 11:26:06 +0900

    fix(WP-283): MileageTransactionHistoryResponse의 JsonNode를 Jackson3로 통일

--- src/main/java/com/gnagnoohc/scms/domain/mileage/DTO/MileageTransactionHistoryResponse.java
+++ src/main/java/com/gnagnoohc/scms/domain/mileage/DTO/MileageTransactionHistoryResponse.java
@@ -1,6 +1,6 @@
 package com.gnagnoohc.scms.domain.mileage.DTO;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import tools.jackson.databind.JsonNode;
 
 import java.math.BigDecimal;
 import java.time.Instant;
